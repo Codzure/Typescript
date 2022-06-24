b3fb44b32e6457abf23c500f77eded72f5fa6c8f
Leonard Muthoni <leonard.muthoni@equitybank.co.ke>
2022-06-24 14:00:49 +0300

    Typescript Strings

---
+++ strings.js
@@ -0,0 +1,3 @@
+var message = "Leonard";
+console.log("A long message to " + message + " filled with text");
+console.log("A long message to ".concat(message, " filled with text"));
---
+++ strings.ts
@@ -0,0 +1,10 @@
+
+var message: string = "Leonard";
+
+console.log(
+  "A long message to " + message + " filled with text"
+);
+
+console.log(
+  `A long message to ${message} filled with text`
+);
